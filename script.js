36c789205ca9121f5ae079b5f497558d9bfac835
JordanPenaloza <jordan332033@gmail.com>
2023-11-18 15:44:02 -0800

    added a lot of shit
    delete function
    check function
    various css bullshit

--- script.js
+++ script.js
@@ -27,35 +27,69 @@ function addItem() {
     newItem.appendChild(itemContent)
 
     // if textbox is empty, do nothing
-    if (textinput.value === '') return
+    if (textinput.value === '') {
+        // alert("Please input a task!");
+        return
+    } else {
+        // create a button to check off items
+        const checkbutton = document.createElement("button")
+        // makes the text inside button to say check
+        checkbutton.innerHTML = '<i class="fa-solid fa-check"></i>'
+        // adds class
+        checkbutton.classList.add("check-button")
+        // adds checkButton to the newItem
+        newItem.appendChild(checkbutton)
 
-    // create a button to check off items
-    const checkbutton = document.createElement("button")
-    // makes the text inside button to say check
-    checkbutton.innerHTML = 'check'
-    // adds class
-    checkbutton.classList.add("check-button")
-    // adds checkButton to the newItem
-    newItem.appendChild(checkbutton)
+        // creates a button to delete items
+        const trashbutton = document.createElement("button")
+        // makes the text inside to say trash
+        trashbutton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+        // adds class
+        trashbutton.classList.add("trash-button")
+        // add trashbutton to the newItem
+        newItem.appendChild(trashbutton)
 
-    // creates a button to delete items
-    const trashbutton = document.createElement("button")
-    // makes the text inside to say trash
-    trashbutton.innerHTML = 'trash'
-    // adds class
-    trashbutton.classList.add("trash-button")
-    // add trashbutton to the newItem
-    newItem.appendChild(trashbutton)
+        // add the new item to the list of items
+        itemlist.appendChild(newItem)
+        // reset the text in textbox to blank to prep next input
+        textinput.value = ''
+    }
+}   
 
-    // add the new item to the list of items
-    itemlist.appendChild(newItem)
-    // reset the text in textbox to blank to prep next input
-    textinput.value = ''
+function deleteItem(event) {
+    // the item focused is the item that was clicked on
+    const item = event.target
+    if(item.classList[0] === "trash-button") {
+        // todolistitem is set the the items parent eleemnt
+        const todolistitem = item.parentElement
+        // remove item
+        todolistitem.remove()
+    }
 }
 
-function deleteItem() {
-    
+function checkItem(event) {
+    // the item focused is the item that was clicked on
+    const item = event.target
+    // if the item clicked has the class "trashbutton"
+    if(item.classList[0] === ("check-button")) {
+        // todolistitem is set the the items parent eleemnt
+       const todolistitem = item.parentElement
+       // toggles the class 'checked' on or off
+       todolistitem.classList.toggle('checked')
+    }
 }
-
+// Function to save the 
+// function saveData(){
+//     localStorage.setItem("data", todolist.innerHTML);
+// }
+// 
+// Function to show the data from
+// function showTask() {
+//     todolist.innerHTML = localStorage.getItem("data");
+//}
+//showTask();
+//
 // checks for button clicks
 buttoninput.addEventListener('click', clickButton)
+itemlist.addEventListener('click', deleteItem)
+itemlist.addEventListener('click', checkItem)
