78166a689f03f73d945d8740cc8de4eb035890ae
ReiLaciste <146046177+ReiLaciste@users.noreply.github.com>
2023-11-17 23:08:51 -0800

    add comments and changed variable names

--- script.js
+++ script.js
@@ -1,41 +1,61 @@
 // Grabs text
-const inputtdl = document.querySelector('.textarea');
-const buttontdl = document.querySelector('.buttoninput');
-const listtdl = document.querySelector('.todolist');
+const textinput = document.querySelector('.textarea');
+// Grabs button input
+const buttoninput = document.querySelector('.buttoninput');
+// Grabs the list of items in todo list
+const itemlist = document.querySelector('.todolist');
 
 function clickButton(event) {
-    // what happens when button is clicked
+    // function to call addItem
     event.preventDefault();
     addItem()
 } 
 
 function addItem() {
-    const itemall = document.createElement('div')
-    itemall.classList.add('itemall')
+    // creates a an element for the new item
+    const newItem = document.createElement('div')
+    // adds a class 'newItem' to the element
+    newItem.classList.add('newItem')
     
-    const item = document.createElement('p');
-    item.classList.add('item')
-    item.innerText = inputtdl.value
-    itemall.appendChild(item)
+    // creates an element for the text in our item
+    const itemContent = document.createElement('p');
+    // adds a class 'item' to the element
+    itemContent.classList.add('itemContent')
+    // the text of the element 'p' is what the text is in our query selector
+    itemContent.innerText = textinput.value
+    // add the itemContent to the newItem div
+    newItem.appendChild(itemContent)
 
-    if (inputtdl.value === '') return
+    // if textbox is empty, do nothing
+    if (textinput.value === '') return
 
+    // create a button to check off items
     const checkbutton = document.createElement("button")
-    checkbutton.innerHTML = '<p> check </p>'
+    // makes the text inside button to say check
+    checkbutton.innerHTML = 'check'
+    // adds class
     checkbutton.classList.add("check-button")
-    itemall.appendChild(checkbutton)
+    // adds checkButton to the newItem
+    newItem.appendChild(checkbutton)
 
+    // creates a button to delete items
     const trashbutton = document.createElement("button")
-    trashbutton.innerHTML = '<p> trash </p>'
+    // makes the text inside to say trash
+    trashbutton.innerHTML = 'trash'
+    // adds class
     trashbutton.classList.add("trash-button")
-    itemall.appendChild(trashbutton)
+    // add trashbutton to the newItem
+    newItem.appendChild(trashbutton)
 
-    listtdl.appendChild(itemall)
-    inputtdl.value = ''
+    // add the new item to the list of items
+    itemlist.appendChild(newItem)
+    // reset the text in textbox to blank to prep next input
+    textinput.value = ''
 }
 
 function deleteItem() {
     
 }
 
-buttontdl.addEventListener('click', clickButton)
+// checks for button clicks
+buttoninput.addEventListener('click', clickButton)
